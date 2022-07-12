23b6408e3719bad82cbba3310e21cc975a80c4ba
unknown <zinlinlatt2002@gmail.com>
2022-07-12 18:51:35 +0630

    momo

---
+++ index.js
@@ -0,0 +1,56 @@
+const countMin  =document.querySelector("#countMin");
+const countSec  =document.querySelector(".countSec");
+const alarmAudio  =document.getElementById("alarm");
+alarmAudio.style.display = "none";
+
+
+
+let count  = 0;
+let timerId =0;
+
+function countingStart(){
+    console.log("TIMEr", timerId);
+    if (timerId !== 0) return;
+    timerId  = setInterval(function() {
+    count++;
+    updateText(count);
+    if(count % 60 === 0){
+        playAlarm();
+    }
+}, 1000);
+console.log("timer ID", timerId);
+}
+function countingStop(){
+    countSec.innerText = "00";
+    clearInterval(timerId);
+    timerId=0;
+    count=0;
+    updateText(count);
+   
+}
+
+function updateText(timer){
+    const min =Math.floor(timer/60);
+    const sec = timer %60;
+    countMin.innerText = min<10 ?   `0${min}`:min;
+    countSec.innerText = sec <10 ?  `0${sec}`:sec;
+  }
+
+
+  function playAlarm(){
+    alarmAudio.play();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
